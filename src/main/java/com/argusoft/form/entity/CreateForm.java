ee26bceccdd5b48183f5c5281a2c519f27c5900c
pratikbs2002 <pratiksuthar1020@gmail.com>
2024-10-08 15:49:21 +0530

    Add: CreateForm and FillForm Class File

---
+++ src/main/java/com/argusoft/form/entity/CreateForm.java
@@ -0,0 +1,43 @@
+package com.argusoft.form.entity;
+
+import java.time.LocalDateTime;
+
+public class CreateForm {
+
+    private Long id;
+    private Integer adminId;
+    private String questions;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(String questions) {
+        this.questions = questions;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/argusoft/form/entity/FillForm.java
@@ -0,0 +1,61 @@
+package com.argusoft.form.entity;
+
+import java.time.LocalDateTime;
+
+public class FillForm {
+
+    private Integer formId;
+    private Integer userId;
+    private String answers;
+    private LocalDateTime createdAt;
+    private String location;
+    private Integer locationId;
+
+    public Integer getFormId() {
+        return formId;
+    }
+
+    public void setFormId(Integer formId) {
+        this.formId = formId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(String answers) {
+        this.answers = answers;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Integer locationId) {
+        this.locationId = locationId;
+    }
+}
